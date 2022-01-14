ade300d64f02e119af4dcb9cced0602698f145b0
GyungA <gga612@naver.com>
2022-01-14 17:43:00 +0900

    test 2

--- src/main/java/com/travel/demo/DemoApplication.java
+++ src/main/java/com/travel/demo/DemoApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)z {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("test");
 	}
-
+// 테스트용
 }
