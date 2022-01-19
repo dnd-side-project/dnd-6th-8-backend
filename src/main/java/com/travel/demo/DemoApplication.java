612bd01f4fd313bdc396985bff849887754db451
jola7373 <jola7373@naver.com>
2022-01-20 02:44:18 +0900

    Feat: arrange  project structure

--- src/main/java/com/travel/demo/DemoApplication.java
+++ src/main/java/com/travel/demo/DemoApplication.java
@@ -2,12 +2,13 @@ package com.travel.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication{
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+        SpringApplication.run(DemoApplication.class, args);
 
+    }
 }
