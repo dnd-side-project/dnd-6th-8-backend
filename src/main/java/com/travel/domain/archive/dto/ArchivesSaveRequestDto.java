da6b5f288ad7e03c5c617e6bcf6dd7ac92ff031b
jola7373 <jola7373@naver.com>
2022-01-30 21:02:29 +0900

    fix: merge files

--- src/main/java/com/travel/domain/archive/dto/ArchivesSaveRequestDto.java
+++ src/main/java/com/travel/domain/archive/dto/ArchivesSaveRequestDto.java
@@ -1,29 +1,37 @@
 package com.travel.domain.archive.dto;
 
 import com.travel.domain.archive.entity.Archives;
+import com.travel.domain.archive.entity.EPlaces;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
-//import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
 
 @Getter
 @ApiModel(value = "태그 생성 정보")
 public class ArchivesSaveRequestDto {
 
-    @ApiModelProperty(value="태그의 이름", example = "이름입니다.", required = true)
-//    @NotBlank(message = "태그 이름이 입력되지 않았습니다.")
+    @ApiModelProperty(value = "아카이브 제목", example = "대충 다녀도 아름다운 제주", required = true)
     private String title;
 
+    @ApiModelProperty(value = "여행 장소", example = "부산", required = true)
+    private EPlaces place;
+
+    @ApiModelProperty(value = "출발 날짜", example = "2021.12.10",required = true)
+    private Date firstDate;
+
     @Builder
-    public ArchivesSaveRequestDto(String title){
+    public ArchivesSaveRequestDto(String title, EPlaces place){
         this.title = title;
+        this.place = place;
     }
 
     public ArchivesSaveRequestDto() {}
 
     public Archives toEntity(){
-        return Archives.builder().title(title).build();
+        return Archives.builder().title(title).place(place).build();
     }
 }
