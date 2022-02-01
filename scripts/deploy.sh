2d1afc5e25d1a9765905724267233d832b271cef
jola7373 <jola7373@naver.com>
2022-02-01 15:30:56 +0900

    chore: add deploy.sh file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ec2-user/app/deploy/*.jar)
-JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
 
-echo "> build 파일 복사" >> /home/ec2-user/action/deploy.log
-DEPLOY_PATH=/home/ec2-user/action/
-cp $BUILD_JAR $DEPLOY_PATH
+REPOSITORY=/home/ec2-user/app/deploy
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+echo">check running pid"
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/deploy.log
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ]; then
+    echo "> There is no pid running currently."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log 
+echo "> deploy new application"
+
+echo "> copy build file"
+
+cp $REPOSITORY/*.jar $REPOSITORY/jar/
+
+JAR_NAME=$(ls -tr $REPOSITORY/jar/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> give authority to $JAR_NAME"
+
+chmod +x $JAR_NAME
+
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
