3052355120601a5945623df1292e1ab27bb47886
GyungA <gga612@naver.com>
2022-06-05 11:21:01 +0900

    fix: Error that can't find DAY By Archives

--- src/main/java/com/travel/domain/day/entity/Days.java
+++ src/main/java/com/travel/domain/day/entity/Days.java
@@ -15,6 +15,8 @@ public class Days {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "DAY")
     private Integer dayNumber;
 
 //    @Column()
@@ -47,7 +49,7 @@ public class Days {
 
     @Builder
     public Days(Integer dayNumber, LocalDate date, String weather, String image, String travelDescription, String emotionDescription, String tipDescription) {
-//        this.dayNumber = dayNumber;
+        this.dayNumber = dayNumber;
 //        this.date = date;
 //        this.weather = weather;
 //        this.image = image;
