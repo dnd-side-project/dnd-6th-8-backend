7ab249ae2c714f03514c94c84a2ad3873cbc1640
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-06-07 22:25:20 +0900

    feat: add setter in DayTotalRequestDto

--- src/main/java/com/travel/domain/day/dto/DayTotalRequestDto.java
+++ src/main/java/com/travel/domain/day/dto/DayTotalRequestDto.java
@@ -1,10 +1,12 @@
 package com.travel.domain.day.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
 public class DayTotalRequestDto {
     private List<DaysSaveRequestDto> daysSaveRequestDto;
 }
