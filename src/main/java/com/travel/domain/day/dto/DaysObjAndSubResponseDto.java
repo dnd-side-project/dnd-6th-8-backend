3e8fc7b3b24fe5bbe78ad77f1e30a188a958afdb
GyungA <gga612@naver.com>
2022-06-08 17:33:08 +0900

    feat: add contents of DaysObjAndSubResponseDto

--- src/main/java/com/travel/domain/day/dto/DaysObjAndSubResponseDto.java
+++ src/main/java/com/travel/domain/day/dto/DaysObjAndSubResponseDto.java
@@ -3,7 +3,11 @@ package com.travel.domain.day.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class DaysObjAndSubResponseDto {
+    private List<DaysObjectiveResponseDto> daysObjectiveResponseDtoList;
+    private List<DaysSubjectiveResponseDto> daysSubjectiveResponseDtoList;
 }
