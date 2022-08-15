780e841386b6164a61e7e63b605f91c5715c9d51
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-08-15 23:48:22 +0900

    [Test] cors 전체 허용

--- src/main/java/com/travel/config/WebMvcConfig.java
+++ src/main/java/com/travel/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*") // 임시로 cors 전체 허용
                 .allowedMethods("*") // 기타 설정
                 .allowedHeaders("*");
     }
